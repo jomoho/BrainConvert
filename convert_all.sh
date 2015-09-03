3e4db4a50e115c5dfd29e4a1f52f0b1be979b861
Moritz Laass <laass@cip.ifi.lmu.de>
2015-09-03 15:16:37 +0200

    converted all

---
+++ convert_all.sh
@@ -0,0 +1,11 @@
+
+./BrainConvert.jar p1_config.txt 
+./BrainConvert.jar p2_config.txt 
+./BrainConvert.jar p3_config.txt 
+./BrainConvert.jar p4_config.txt 
+./BrainConvert.jar p5_config.txt 
+./BrainConvert.jar p6_config.txt 
+./BrainConvert.jar p7_config.txt 
+./BrainConvert.jar p8_config.txt 
+./BrainConvert.jar p9_config.txt 
+./BrainConvert.jar p10_config.txt
